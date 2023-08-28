faab051a941de26c723108aec4638f79f9042c23
Naudy Castellanos <naudycastellanos@ingenieros.com>
2023-08-28 16:21:13 +0100

    add comments for added previewer version 2

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import v1roomsRoutes from './v1/routes/rooms.routes.js';
 import v1indexRoutes from './v1/routes/index.routes.js';
+
+// preview v2 unchanged compared to version 1 [ 28-08-2023 ]
+// ><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><
 import v2roomsRoutes from './v2/routes/rooms.routes.js';
 import v2indexRoutes from './v2/routes/index.routes.js';
+// ><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><
 
 const app = express ()
 
@@ -10,8 +14,13 @@ app.use( express.json() )
 
 app.use ( v1indexRoutes )
 app.use ( '/api/v1', v1roomsRoutes )
+
+// preview v2 unchanged compared to version 1 [ 28-08-2023 ]
+// ><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><
 app.use ( v2indexRoutes )
 app.use ( '/api/v2', v2roomsRoutes )
+// ><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><
+
 
 app.use((req, res, next) => {
 
